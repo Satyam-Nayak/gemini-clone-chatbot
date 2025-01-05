666e1ad476018cdd0b88097a56c66b364fb800cc
Satyam-Nayak <satyamn120@gmail.com>
2025-01-05 21:28:52 +0530

    add icons to the webpage

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -7,6 +7,17 @@ const Sidebar = () => {
     <div className='sidebar'>
       <div className="top">
         <img className='menu' src={assets.menu_icon} alt="" />
+        <div className='new-chat'>
+          <img src={assets.plus_icon} alt="plus-icon" />
+          <p>New Chat</p>
+        </div>
+        <div className="recent">
+          <p className="recent-title">Recent</p>
+          <div className="recent-entry">
+            <img src={assets.message_icon} alt="" />
+            <p>What is react</p>
+          </div>
+        </div>
       </div>
       <div className="bottom">
 
